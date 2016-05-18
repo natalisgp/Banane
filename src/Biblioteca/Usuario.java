cdd3f6c749bb09ed7e1293555391d2aa71263e0e
Natalia <natalisgp@gmail.com>
2016-05-18 11:58:21 +0200

    prueba

--- src/Biblioteca/Usuario.java
+++ src/Biblioteca/Usuario.java
@@ -2,7 +2,7 @@ package Biblioteca;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
+//
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
